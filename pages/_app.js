ff95c16daf239676c2910d0844d776d5da6cf803
abhik <avik5324@gmail.com>
2021-07-31 21:36:58 +0530

    gAnalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+
 import { ThemeProvider } from 'next-themes'
 import { MDXProvider } from '@mdx-js/react'
 
@@ -10,14 +14,24 @@ import MDXComponents from './../components/MDXComponents'
 
 function MyApp({ Component, pageProps }) {
 
+  const router = useRouter()
+
   initFirebase()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={MDXComponents}>
-        
           <Component {...pageProps} />
-        
       </MDXProvider>
     </ThemeProvider>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -11,6 +13,23 @@ class MyDocument extends Document {
       <Html className="">
         <Head >
           <link rel="icon" href="/static/images/favicon.ico" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body className="dark:bg-gray-900 dark:text-gray-50 lg:text-lg md:text-md">
           <Main />
