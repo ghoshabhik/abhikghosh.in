4f445ec7fe18dfc94b712e5b80ada8917bdf5b53
abhik <avik5324@gmail.com>
2021-08-02 23:26:47 +0530

    added activity component

---
+++ components/Activities.js
@@ -0,0 +1,37 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import { useState } from 'react'
+import { formatDistance } from 'date-fns'
+
+export default function Activities() {
+
+    const [activities, setActivities] = useState([])
+
+    firebase.firestore()
+                .collection('activities').onSnapshot((snapshot) => {
+                    const data = snapshot.docs.map((doc) => ({
+                      id: doc.id,
+                      ...doc.data(),
+                    }));
+                    // console.log("All data in 'activities' collection", data)
+                    setActivities(data)
+                  });
+    // console.log('activities --',activities)
+
+    return (
+        <div className=" border dark:border-gray-600 border-gray-200 rounded-md px-3 py-2 mb-3 h-96 overflow-x-hidden overflow-y-auto no-scrollbar">
+           <div className="flex space-x-2 items-center my-4 font-semibold">
+           <p><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="h-6 w-6" viewBox="0 0 24 24" viewBox="0 0 24 24"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1 18h-2v-8h2v8zm-1-12.25c.69 0 1.25.56 1.25 1.25s-.56 1.25-1.25 1.25-1.25-.56-1.25-1.25.56-1.25 1.25-1.25z"/></svg></p>
+           <p>Recent Activities</p> 
+           </div> 
+           {activities?.map( (activity, index) => {
+                    return(
+                    <div key={index} className="flex flex-col justify-between mb-4">
+                        <p className="text-black dark:text-gray-100"><a href={activity.url} rel="nofollow noreferrer"  target="_blank">{activity.message}</a> - <span className="text-md text-gray-500 dark:text-gray-400">{formatDistance(
+                        activity.createdAt.toDate(),  new Date(),  { addSuffix: true })}</span></p>
+                        
+                    </div>)
+            })}
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Tweets from "../components/Tweets"
 import Instagram from "../components/Instagram"
 import ProfileLinks from "../components/ProfileLinks"
 import Container from "../components/Container"
+import Activities from "../components/Activities"
 
 export default function Home() {
   return (
@@ -11,14 +12,19 @@ export default function Home() {
       <Container
       title="Home for Personal Website/ Developer Portfolio – Abhik Ghosh">
         Index
-        
+        <div className="grid xl:grid-cols-3 grid-cols-1 gap-2">
+        <div className="xl:col-span-2 h-96">Blog and Project Section</div>
+        <Activities />
+        </div>
         <div className="flex flex-col md:flex-row md:space-x-2 space-y-2 md:space-y-0 mb-3">
           <CurrentlyListening />
           <CurrentlyReading />
         </div>
+        
         <Tweets />
         <Instagram />
         <ProfileLinks />
+        
       </Container>
     </div>
   )
