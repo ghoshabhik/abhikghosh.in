5a22c3c50af6dfa5c8cbc1061cac9cc55082224a
Abhik Ghosh <avik5324@gmail.com>
2021-08-07 20:28:27 +0530

    removed the unused block from dash page

--- pages/recents.js
+++ pages/recents.js
@@ -22,15 +22,18 @@ export default function Recents() {
               My Dashboards
         </h1>
         <p className="text-gray-600 dark:text-gray-400 mb-4">
-              Here on this page, you can see things like a quick Github dashboard and few other data pulled in to a single page. The songs Abhik is listening to, the books he is reading, the recent posts on Instagrams, and activites on Twitter etc. This is an experimental page to try out various api integrations and curate some meaningful data story. 
+              Here on this page, you can see things like a quick Github dashboard and few other data pulled in to a single 
+              page. The songs I am listening to, the books I am reading, the recent posts on Instagrams, and activites on 
+              Twitter etc. This is an experimental page to try out various api integrations and curate some meaningful 
+              data story. 
         </p>
         </div>
         
-        <div className="grid xl:grid-cols-3 grid-cols-1 gap-2">
+        {/* <div className="grid xl:grid-cols-3 grid-cols-1 gap-2">
         <div className="xl:col-span-2 h-96">Blog and Project Section</div>
 
         <div>something else</div>
-        </div>
+        </div> */}
         <div className="border border-gray-300 dark:border-gray-700 rounded-lg py-10 px-2 md:px-10 my-3">
             <GithubStats />
             </div>
